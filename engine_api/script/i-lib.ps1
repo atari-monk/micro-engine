d4adeb85282ffd27ea881330f8bc25c2c9cfbbb7
atari-monk <atari.monk1@gmail.com>
2023-11-14 18:29:15 +0100

    engine_api start

---
+++ engine_api/script/i-lib.ps1
@@ -0,0 +1,15 @@
+. "C:\atari-monk\Code\micro-engine\script\ps1\CommonFunctions.ps1"
+
+$libName = "engine_api"
+$build = "$libName\build\"
+$pack = "$libName-1.0.0.tgz"
+$lib = $RepoPath + $build + $pack
+
+$targetProjects = @(
+  "$RepoPath\engine\"
+)
+
+Build-Lib
+foreach ($targetProj in $targetProjects) {
+  Copy-And-Install-Pack -packPath $lib -projDir $targetProj -packName $pack
+}
